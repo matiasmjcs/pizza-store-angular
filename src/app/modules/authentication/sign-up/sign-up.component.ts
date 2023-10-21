a98160b0c41a8acb7a86482b89570b2aaba52280
matiasmjcs <matiasmejiascisternas@gmail.com>
2023-10-21 08:22:22 -0300

    #	modified:   src/app/modules/authentication/sign-up/sign-up.component.html #	modified:   src/app/modules/authentication/sign-up/sign-up.component.scss #	modified:   src/app/modules/authentication/sign-up/sign-up.component.ts

--- src/app/modules/authentication/sign-up/sign-up.component.ts
+++ src/app/modules/authentication/sign-up/sign-up.component.ts
@@ -1,11 +1,29 @@
 import { Component } from '@angular/core';
-
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthenticationService } from '../authentication.service';
 @Component({
-  standalone: true,
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
   styleUrls: ['./sign-up.component.scss']
 })
 export class SignUpComponent {
+  signUpForm: FormGroup
+  constructor(private fb: FormBuilder, private authenticationService: AuthenticationService) {
+    this.signUpForm = this.fb.group({
+      username: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    })
+  }
 
+  signup() {
+    this.authenticationService.signup(this.signUpForm.value.username, this.signUpForm.value.email, this.signUpForm.value.password).subscribe(
+      (response: any) => {
+        console.log(response);
+      },
+      (error: any) => {
+        console.error(error);
+      }
+    )
+  }
 }
